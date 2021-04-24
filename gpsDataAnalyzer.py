f999f6cc1659c1261b2fbf4d09d35b695dc1c38c
Paula-15 <paulataibo15@gmail.com>
2021-04-24 10:17:35 +0200

    Modificacion gpsAnalizer.py

--- gpsDataAnalyzer.py
+++ gpsDataAnalyzer.py
@@ -13,14 +13,29 @@ def parse_NMEA_line (line):
 	NMEA_fields = NMEA_data[0].split(',')
 
 	if NMEA_fields[0] == "$GPGGA":
+		rawLat = NMEA_fields[2]
+		latDeg = rawLat[:2]
+		latMin = rawLat[3:]
+		print (len(latDeg))
+		print ("Deg:" + latDeg)
+		print ("Min:" + latMin)
+#		print (latTry)
+
+		'''
+		rawLat = str(NMEA_fields[2])
+		print (rawLat)
+		latitud = rawLat[2:] + (rawLat[:3] / 60)
+		rawLon = str(NMEA_fields[4])
+		longitud = rawLon[2:] + (rawLon[:3] / 60)
+
 		outputLog.write(NMEA_fields[0] + ":\n")
 		outputLog.write ("\tTime: " +  NMEA_fields[1] + "\n")
-		outputLog.write ("\tLat: " + NMEA_fields[2] + " " + NMEA_fields[3] + "\n")
-		outputLog.write ("\tLon: " + NMEA_fields[4] + " " + NMEA_fields[5] + "\n")
+		outputLog.write ("\tLat: " + latitud + " " + NMEA_fields[3] + "\n")
+		outputLog.write ("\tLon: " + longitud + " " + NMEA_fields[5] + "\n")
 		outputLog.write ("\tQuality: " + NMEA_fields[6] + "\n")
 		outputLog.write ("\tSatélites: " + NMEA_fields[7] + "\n")
 		outputLog.write ("\tAltitud: " + NMEA_fields[9] + " " + NMEA_fields[10] + "\n")
-
+		'''
 	elif NMEA_fields[0] == "$GPGLL":
 		outputLog.write (NMEA_fields[0] + ":\n")
 		outputLog.write ("\tLat: " + NMEA_fields[1] + " " + NMEA_fields[2] + "\n")
@@ -56,13 +71,13 @@ def parse_NMEA_line (line):
 
 
 
-#lineCount = 1
+lineCount = 1
 
 with open(filename) as f:
         content = f.readlines()
 
 for line in content:
-#	print(lineCount)
-#	lineCount += 1
+	print(lineCount)
+	lineCount += 1
 	if is_valid_NMEA_command(line):
 		parse_NMEA_line(line)
